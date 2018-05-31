dd137aa04d1be32962cf0d455b99d8ddd28a71dd
Andrey Shovkoplyas <motor4ik@gmail.com>
2018-05-31 11:19:47 +0300

    improved new chat UI

--- src/status_im/ui/screens/add_new/styles.cljs
+++ src/status_im/ui/screens/add_new/styles.cljs
@@ -3,14 +3,27 @@
   (:require [status-im.ui.components.colors :as colors]
             [status-im.ui.components.styles :as styles]))
 
+(def new-chat-container
+  {:flex-direction :row
+   :align-items    :center})
+
 (def input-container
-  {:flex-direction    :row
-   :align-items       :center
-   :border-radius     styles/border-radius
-   :height            52
-   :background-color  colors/gray-lighter
-   :margin-horizontal 14
-   :margin-top        24})
+  {:flex             1
+   :border-radius    styles/border-radius
+   :height           52
+   :background-color colors/gray-lighter
+   :margin-left      14
+   :margin-top       24})
+
+(def button-container
+  {:justify-content    :center
+   :border-radius      styles/border-radius
+   :height             52
+   :background-color   colors/gray-lighter
+   :padding-horizontal 15
+   :margin-right       14
+   :margin-left        3
+   :margin-top         24})
 
 (defstyle input
   {:flex               1
